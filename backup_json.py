ac689d855d0c24cb15c262b0191ab5ee65595463
kingcos <2821836721v@gmail.com>
2022-09-08 00:57:55 +0800

    Support OAuth2 login & get current user id

--- backup_json.py
+++ backup_json.py
@@ -4,6 +4,7 @@ import sys
 import json
 import time
 import oauth2
+import urllib
 import datetime
 import requests
 
@@ -11,17 +12,49 @@ import requests
 class FanFou:
     host = "fanfou.com"
 
-    def __init__(self, api_key, api_secret, token, token_secret):
+    def __init__(self, api_key, api_secret, username, password, user_id=''):
         self.api_key = api_key
         self.api_secret = api_secret
-        self.token = token
-        self.token_secret = token_secret
+        self.token, self.token_secret = self.login(username, password)
+        if user_id == '':
+            self.user_id = self.get_current_user_id()
 
-    def request_user_timeline(self, _id, max_id='', count=1):
+    def login(self, username, password):
+        print('------ login ------')
+        params = {'x_auth_username': username, 'x_auth_password': password, 'x_auth_mode': 'client_auth'}
+        url = "http://fanfou.com/oauth/access_token?{}".format(urllib.parse.urlencode(params))
+
+        consumer = oauth2.Consumer(self.api_key, self.api_secret)
+        client = oauth2.Client(consumer)
+        client.add_credentials(username, password)
+        client.set_signature_method(oauth2.SignatureMethod_HMAC_SHA1())
+        resp, token_bytes = client.request(url)
+        tokens = dict(urllib.parse.parse_qsl(token_bytes.decode("utf-8")))
+
+        if len(tokens) == 2:
+            return tokens['oauth_token'], tokens['oauth_token_secret']
+        else:
+            print('登录失败！')
+            exit(-1)
+
+    def get_current_user_id(self):
+        print('------ get_current_user_id ------')
+        url = 'http://api.fanfou.com/account/verify_credentials.json'
+        params = {'mode': 'lite'}
+
+        consumer = oauth2.Consumer(self.api_key, self.api_secret)
+        token = oauth2.Token(self.token, self.token_secret)
+        client = oauth2.Client(consumer, token)
+
+        response, content = client.request(url, method='POST', body=urllib.parse.urlencode(params))
+        result = json.loads(content)
+        return result['id']
+
+    def request_user_timeline(self, max_id='', count=1):
         print('------ request_user_timeline ------')
-        url = f"http://api.fanfou.com/statuses/user_timeline.json?id={_id}&count={count}&format=html"
+        url = f"http://api.fanfou.com/statuses/user_timeline.json?id={self.user_id}&count={count}&format=html"
         if len(max_id):
-            url = f"http://api.fanfou.com/statuses/user_timeline.json?max_id={max_id}&id={_id}&count={count}&format=html"
+            url = f"http://api.fanfou.com/statuses/user_timeline.json?max_id={max_id}&id={self.user_id}&count={count}&format=html"
 
         consumer = oauth2.Consumer(self.api_key, self.api_secret)
         token = oauth2.Token(self.token, self.token_secret)
@@ -89,12 +122,12 @@ class FileUtil:
             f.write(content)
 
 
-def find_all_content(ff, _id, content_list=[], append_list=[], count=40):
+def find_all_content(ff, content_list=[], append_list=[], count=40):
     max_id = append_list[0]['id'] if len(append_list) > 0 else ''
     max_id_to = content_list[-1]['id'] if len(content_list) > 0 else ''
 
     time.sleep(5)
-    ts = ff.request_user_timeline(count=count, max_id=max_id, _id=_id)
+    ts = ff.request_user_timeline(count=count, max_id=max_id)
 
     content_list_ids = list(map(lambda x: x['id'], content_list))
     append_list_ids = list(map(lambda x: x['id'], append_list))  # 1, 2, 3，时间正序（旧 -> 新）
@@ -120,37 +153,30 @@ def find_all_content(ff, _id, content_list=[], append_list=[], count=40):
         # 到达尾部
         return append_list
 
-    return find_all_content(ff, _id, content_list, append_list, count)
+    return find_all_content(ff, content_list, append_list, count)
 
 
 if __name__ == '__main__':
-    # 0. 参数校验
-    if len(sys.argv) != 2:
-        print("参数个数有误！")
-        exit(-1)
-
-    user_id = sys.argv[1]
-
     api_key = os.environ['API_KEY']
     api_secret = os.environ['API_SECRET']
 
-    token = os.environ['TOKEN']
-    token_secret = os.environ['TOKEN_SECRET']
+    username = os.environ['USERNAME']
+    password = os.environ['PASSWORD']
 
-    # 0.1 测试
-    # test_token = sys.argv[6]
-    # print(test_token)
+    # 0.1 获取 user_id
+    user_id = sys.argv[1] if len(sys.argv) > 1 else ''
 
     # 0.2 构造对象
     fu = FileUtil('fanfou.json')
-    ff = FanFou(api_key, api_secret, token, token_secret)
+    ff = FanFou(api_key, api_secret, username, password, user_id)
+
 
     # 1. 读取原文件内容
     content_list = fu.get_json_list()
     # print(content_list)
 
     # 2. 添加完整内容
-    content_list = find_all_content(ff, _id=user_id, content_list=content_list)
+    content_list = find_all_content(ff, content_list)
     print('总计 ' + str(len(content_list)) + ' 条内容')
 
     # 3. 写回文件，再读取

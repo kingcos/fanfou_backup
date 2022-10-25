3741742c03667098e4b19f043005297604d64552
kingcos <2821836721v@gmail.com>
2022-10-25 09:34:16 +0800

    Test

--- fanfou_backup/src/pages/MainPage.vue
+++ fanfou_backup/src/pages/MainPage.vue
@@ -1,32 +1,50 @@
 <script setup lang="ts">
-import { requestFanfous, type Fanfou } from "../utils/fanfou";
+// import { requestFanfous, type Fanfou } from "../utils/fanfou";
 import { reactive } from "vue";
 
 console.log("--- RUNNING ---");
 
-const rangeArray = (start: number, end: number) => {
-  console.log("rangeArray", start, end);
-  const arr = Array(end - start + 1)
-    .fill(0)
-    .map((v, i) => i + start);
-  console.log("rangeArray", arr);
-  return arr;
-};
+// const rangeArray = (start: number, end: number) => {
+//   console.log("rangeArray", start, end);
+//   const arr = Array(end - start + 1)
+//     .fill(0)
+//     .map((v, i) => i + start);
+//   console.log("rangeArray", arr);
+//   return arr;
+// };
 
-const PER_PAGE: number = 15;
+// const PER_PAGE: number = 15;
+
+// const data = reactive<{
+//   fanfous: Fanfou[];
+//   currentPage: number;
+//   totalPages: number;
+// }>({ fanfous: [], currentPage: 1, totalPages: 0 });
 
 const data = reactive<{
-  fanfous: Fanfou[];
   currentPage: number;
   totalPages: number;
-}>({ fanfous: [], currentPage: 1, totalPages: 0 });
+}>({ currentPage: 1, totalPages: 45 });
 
-// 1: 0,1,2,3,4,5,6,7,8,9 => 10 个内容，0-9
+// // 1: 0,1,2,3,4,5,6,7,8,9 => 10 个内容，0-9
 
-requestFanfous().then((result) => {
-  data.fanfous = result.reverse();
-  data.totalPages = Math.ceil(result.length / PER_PAGE);
-});
+// requestFanfous().then((result) => {
+//   data.fanfous = result.reverse();
+//   data.totalPages = Math.ceil(result.length / PER_PAGE);
+// });
+
+// const pageOperate = (operate: number) => {
+//   const next = data.currentPage + operate;
+//   console.log("pageOperate", next);
+//   if (next <= 0) {
+//     data.currentPage = data.totalPages;
+//   } else if (next > data.totalPages) {
+//     data.currentPage = 1;
+//   } else {
+//     data.currentPage = next;
+//   }
+//   console.log("pageOperate", data.currentPage);
+// };
 
 const pageOperate = (operate: number) => {
   const next = data.currentPage + operate;
@@ -43,30 +61,37 @@ const pageOperate = (operate: number) => {
 </script>
 
 <template>
-  <main v-if="data.fanfous.length">
+  <main>
     <h1>我的饭否备份</h1>
     <h4>
       <a href="https://github.com/kingcos/fanfou_backup"
         >github.com/kingcos/fanfou_backup</a
       >
     </h4>
-    <div
-      class="fanfou"
-      v-for="(index, key) in rangeArray(
-        (data.currentPage - 1) * PER_PAGE,
-        Math.min(data.currentPage * PER_PAGE - 1, data.fanfous.length - 1)
-      )"
-      :key="key"
-    >
-      <!-- <div>{{ index }}</div> -->
-      <div v-if="data.fanfous[index].text.length">
-        <span v-html="data.fanfous[index].text"></span>
+    <!-- <div v-if="data.fanfous.length">
+      <div
+        class="fanfou"
+        v-for="(index, key) in rangeArray(
+          (data.currentPage - 1) * PER_PAGE,
+          Math.min(data.currentPage * PER_PAGE - 1, data.fanfous.length - 1)
+        )"
+        :key="key"
+      >
+        <div v-if="data.fanfous[index].text.length">
+          <span v-html="data.fanfous[index].text"></span>
+        </div>
+        <div v-if="data.fanfous[index].photo">
+          <img :src="data.fanfous[index].photo.largeurl" />
+        </div>
+        <span class="time">{{ data.fanfous[index].created_at }}</span>
       </div>
-      <div v-if="data.fanfous[index].photo">
-        <img :src="data.fanfous[index].photo.largeurl" />
+      <div class="pager">
+        <div @click="pageOperate(-1)" class="last_page">上一页</div>
+        <div class="pages">{{ data.currentPage }} / {{ data.totalPages }}</div>
+        <div @click="pageOperate(1)" class="next_page">下一页</div>
       </div>
-      <span class="time">{{ data.fanfous[index].created_at }}</span>
-    </div>
+    </div> -->
+
     <div class="pager">
       <div @click="pageOperate(-1)" class="last_page">上一页</div>
       <div class="pages">{{ data.currentPage }} / {{ data.totalPages }}</div>

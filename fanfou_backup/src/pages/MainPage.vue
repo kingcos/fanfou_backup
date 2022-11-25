351e2bbca2a77a535db04795860299772f903d39
kingcos <2821836721v@gmail.com>
2022-11-25 20:22:33 +0800

    More features

--- fanfou_backup/src/pages/MainPage.vue
+++ fanfou_backup/src/pages/MainPage.vue
@@ -1,8 +1,17 @@
 <script setup lang="ts">
+import { useRoute, useRouter } from "vue-router";
+import { reactive, ref, watch, computed } from "vue";
 import { requestFanfous, type Fanfou } from "../utils/fanfou";
-import { reactive, ref, watch } from "vue";
 
 console.log("--- RUNNING ---");
+
+const route = useRoute();
+const router = useRouter();
+const currentPage = computed(() => {
+  const page = route.query.page ? Number(route.query.page) : 1;
+  console.log("page ", page);
+  return page;
+});
 
 const rangeArray = (start: number, end: number) => {
   console.log("rangeArray", start, end);
@@ -18,15 +27,14 @@ const PER_PAGE: number = 15;
 const data = reactive<{
   orginFanfous: Fanfou[];
   fanfous: Fanfou[];
-  currentPage: number;
   totalPages: number;
   loading: boolean;
+  image?: string;
 }>({
   orginFanfous: [],
   fanfous: [],
-  currentPage: 1,
   totalPages: 0,
-  loading: true,
+  loading: false,
 });
 
 // // 1: 0,1,2,3,4,5,6,7,8,9 => 10 个内容，0-9
@@ -44,6 +52,7 @@ watch(keyword, (newValue) => {
 });
 
 const refresh = () => {
+  data.loading = true;
   requestFanfous()
     .then((result) => {
       data.loading = false;
@@ -57,16 +66,18 @@ const refresh = () => {
 };
 
 const pageOperate = (operate: number) => {
-  const next = data.currentPage + operate;
+  const next = currentPage.value + operate;
   console.log("pageOperate", next);
+  let page = next
   if (next <= 0) {
-    data.currentPage = data.totalPages;
+    page = data.totalPages;
   } else if (next > data.totalPages) {
-    data.currentPage = 1;
-  } else {
-    data.currentPage = next;
+    page = 1;
   }
-  console.log("pageOperate", data.currentPage);
+  console.log("pageOperate", page);
+  router.push({ query: { page: page } }).then(() => {
+    refresh();
+  });
 };
 
 const formattedDate = (dateString: string) => {
@@ -87,10 +98,18 @@ const formattedDate = (dateString: string) => {
   return year + month + day + hour + minute;
 };
 
+const clickImage = (url?: string) => {
+  console.log(url);
+  data.image = url;
+};
+
 refresh();
 </script>
 
 <template>
+  <div v-if="data.image" @click="clickImage()" class="image_container">
+    <img class="image" :src="data.image" />
+  </div>
   <main>
     <h1>我的饭否备份</h1>
     <h4>
@@ -110,8 +129,8 @@ refresh();
       <div
         class="fanfou"
         v-for="(index, key) in rangeArray(
-          (data.currentPage - 1) * PER_PAGE,
-          Math.min(data.currentPage * PER_PAGE - 1, data.fanfous.length - 1)
+          (currentPage - 1) * PER_PAGE,
+          Math.min(currentPage * PER_PAGE - 1, data.fanfous.length - 1)
         )"
         :key="key"
       >
@@ -119,7 +138,10 @@ refresh();
           <span v-html="data.fanfous[index].text"></span>
         </div>
         <div class="photo" v-if="data.fanfous[index].photo">
-          <img :src="data.fanfous[index].photo.largeurl" />
+          <img
+            :src="data.fanfous[index].photo.largeurl"
+            @click="clickImage(data.fanfous[index].photo.largeurl)"
+          />
         </div>
         <span class="time">{{
           formattedDate(data.fanfous[index].created_at)
@@ -127,7 +149,7 @@ refresh();
       </div>
       <div class="pager">
         <div @click="pageOperate(-1)" class="last_page">上一页</div>
-        <div class="pages">{{ data.currentPage }} / {{ data.totalPages }}</div>
+        <div class="pages">{{ currentPage }} / {{ data.totalPages }}</div>
         <div @click="pageOperate(1)" class="next_page">下一页</div>
       </div>
     </div>
@@ -153,14 +175,19 @@ h1 {
 
   font-size: large;
 
-  img {
-    margin-top: 10px;
-    width: 50%;
-  }
-}
+  .photo {
+    flex: 0 0 33.3%;
+    padding: 5px;
 
-.photo {
-  width: 50%;
+    width: 30%;
+    aspect-ratio: 1;
+
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
+  }
 }
 
 .time {
@@ -195,5 +222,31 @@ h1 {
 
   font-size: medium;
   margin-top: 10px;
+}
+
+.image_container {
+  position: fixed;
+  z-index: 99;
+  width: 100%;
+  height: 100%;
+  background-color: black;
+  left: 0;
+  top: 0;
+
+  display: flex;
+  align-items: center;
+}
+
+.image {
+  padding: 10px;
+  max-width: 100%;
+  max-height: 100%;
+  margin: auto;
+}
+
+@media (prefers-color-scheme: dark) {
+  .search {
+    background-color: black;
+  }
 }
 </style>
